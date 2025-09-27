7de0c13e58226c79a30ae2386cacc13c369a52dc
Ryan Jin <ryanjin333@gmail.com>
2025-09-27 13:20:25 -0400

    frontend structure

--- frontend/app/app.tsx
+++ frontend/app/app.tsx
@@ -1,6 +1,16 @@
-import WelcomeKit from '@/app/components/welcome/WelcomeKit'
 import './styles/app.css'
+import ContentArea from './components/custom/ContentArea'
+import Sidebar from './components/custom/Sidebar'
+import Header from './components/layout/Header'
 
 export default function App() {
-  return <WelcomeKit />
+  return (
+    <div className="flex flex-row w-full">
+      <Sidebar />
+      <div className="flex flex-col w-full">
+        <Header />
+        <ContentArea />
+      </div>
+    </div>
+  )
 }
